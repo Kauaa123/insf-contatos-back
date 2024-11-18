1ae57d1f29c622b32ba8f57fe879d9e97e3510bd
Kauaa123 <ra49014845839@acaonsfatima.org.br>
2024-11-18 14:05:13 -0300

    a

--- src/validation/ValidarContatos.js
+++ src/validation/ValidarContatos.js
@@ -4,6 +4,6 @@ export default function ValidarContatos(req) {
     if(!req.body.titulo) throw new Error('O parâmetro título está inválido.')
     if(!req.body.informacoes) throw new Error('O parâmetro Informações está inválido.')
     if(!req.body.impacto) throw new Error('O parâmetro Impacto está inválido.')
-    if(!req.body.ocorrencia) throw new Error('O parâmetro Ocorrencia está inválido.')    
+    if(req.body.ocorrencia == null) throw new Error('O parâmetro Ocorrencia está inválido.')    
     if(!req.body.atribuir) throw new Error('O parâmetro Atribuir está inválido.')
 }
