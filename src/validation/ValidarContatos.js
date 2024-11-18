1f41d2c45c7be693dbcaec21464daf047d0eb1e3
Kauaa123 <ra49014845839@acaonsfatima.org.br>
2024-11-18 10:47:36 -0300

    commitado

---
+++ src/validation/ValidarContatos.js
@@ -0,0 +1,9 @@
+import { inserirContato } from "../repository/ContatosRepository.js";
+
+export default function ValidarContatos(req) {
+    if(!req.body.titulo) throw new Error('O parâmetro título está inválido.')
+    if(!req.body.informacoes) throw new Error('O parâmetro Informações está inválido.')
+    if(!req.body.impacto) throw new Error('O parâmetro Impacto está inválido.')
+    if(!req.body.ocorrencia) throw new Error('O parâmetro Ocorrencia está inválido.')    
+    if(!req.body.atribuir) throw new Error('O parâmetro Atribuir está inválido.')
+}
